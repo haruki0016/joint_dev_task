ae013e2a3786265bf3fe546e4c088f23f5cb7716
haruki0016 <haruki.ukiuki@gmail.com>
2021-05-14 06:02:32 +0900

    PHP基礎課題Q1-Q7

--- task.php
+++ task.php
@@ -1,3 +1,4 @@
+<?php
 # 課題の回答は このファイル をご利用下さい。
 # 回答の出力を確認される際は，「php task.php」をターミナルから実行して下さい。
 
@@ -5,6 +6,11 @@ print("#####q1#####".PHP_EOL);
 $names = ["田中", "佐藤", "佐々木", "高橋"];
 
   # 以下に回答を記載
+  $names = ["田中", "佐藤", "佐々木", "高橋"];
+
+array_push($names, "斉藤");
+
+print_r($names);
 
 echo PHP_EOL;
 
@@ -13,6 +19,9 @@ $array1 = ["dog", "cat", "fish"];
 $array2 = ["bird", "bat", "tiger"];
 
   # 以下に回答を記載
+  //$array1と$array2を結合
+  $result = array_merge($array1, $array2);
+  print_r($result);
 
 echo PHP_EOL;
 
@@ -20,6 +29,10 @@ print("#####q3#####".PHP_EOL);
 $numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9];
 
   # 以下に回答を記載
+  //＄numbersの中に"3"がいくつあるかを出力
+  $counts = array_count_values($numbers);
+
+  echo $counts["3"] . "回" . PHP_EOL;
 
 echo PHP_EOL;
 
@@ -27,6 +40,10 @@ print("#####q4#####".PHP_EOL);
 $sports = ["サッカー", "フットサル", null, "野球", "バスケ", null, "バレー"];
 
   # 以下に回答を記載
+  //$sportsの中の"null"(重複)の要素を削除して出力
+  $unique = array_unique($sports);
+
+  print_r($unique);
 
 echo PHP_EOL;
 
@@ -35,6 +52,12 @@ $array1 = [];
 $array2 = [1, 5, 8, 10];
 
   # 以下に回答を記載
+  // 配列が空 = true 1つ以上の要素 = false
+  $array1 = [];
+  var_export(empty($array1)) . PHP_EOL;
+
+  $array2 = [1, 5, 8, 10];
+  var_export(empty($array2)) . PHP_EOL;
 
 echo PHP_EOL;
 
@@ -42,6 +65,12 @@ print("#####q6#####".PHP_EOL);
 $numbers1 = [1, 2, 3, 4, 5];
 
   # 以下に回答を記載
+  //$number2で$number1の配列に10をかけた数値を出力
+  $numbers2 = [];
+  foreach ($numbers1 as $number) {
+    $number2[] = $number * 10;
+  }
+  print_r($number2);
 
 echo PHP_EOL;
 
@@ -49,6 +78,8 @@ print("#####q7#####".PHP_EOL);
 $array = ["1", "2", "3", "4", "5"];
 
   # 以下に回答を記載
+  //文字列から数字に変換
+  $array = array_map('intval', $array);
 
   # 以下は変更しないで下さい
 var_dump($array);
@@ -205,3 +236,4 @@ foreach($humans as $human){
 }
 
 echo PHP_EOL;
+?>
